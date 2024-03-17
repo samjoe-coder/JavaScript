3554bdd87f31abfbc5ba5a8f00046fc261163d07
Samuel Joseph Karetla <ksamueljoseph006@gmail.com>
2024-03-17 11:04:32 +0530

    Intro - export and import in commonJS and ES6 modules

---
+++ jsIntro/01exp.js
@@ -0,0 +1,17 @@
+// const math = require("./mathExports");
+
+// console.log(math.add(5, 3)); 
+// console.log(math.sub(5, 3));
+// console.log(math.mul(5, 3));
+// console.log(math.div(5, 0)); 
+// console.log(math.pi); 
+// console.log(math.defaultFun()); 
+
+import { add, sub, mul, div, pi, defaultFun } from "./mathExports.js";
+
+console.log(add(5, 3)); 
+console.log(sub(5, 3));
+console.log(mul(5, 3));
+console.log(div(5, 0)); 
+console.log(pi); 
+console.log(defaultFun()); 
---
+++ jsIntro/mathExports.js
@@ -0,0 +1,28 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    if(b!=0){
+        return a/b;
+    }
+    else{
+        return "Demominator can't be 0"
+    }
+}
+
+const pi = 3.14
+
+function defaultFun(){
+    return "This is a default function";
+}
+
+export { add, sub, mul, div, pi, defaultFun };
